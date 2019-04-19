fe996e653e672458ecc35c4384c044c7c6c01978
Bade99 <31745377+Bade99@users.noreply.github.com>
2019-04-19 01:20:31 -0300

    configuro dos pistas y una funcion para cargarlas facilmente

--- TGC.Group/Model/PistasReferencia.cs
+++ TGC.Group/Model/PistasReferencia.cs
@@ -18,7 +18,7 @@ namespace TGC.Group.Model
     public class PistasReferencia : SceneElement
     {
         private TgcSceneLoader loader;
-        TgcScene scene;
+        TgcScene scene, escena_sin_paredes;
         private int n;
         /// <summary>
         ///     n representa la cantidad de pistas que va a graficar
@@ -27,21 +27,41 @@ namespace TGC.Group.Model
         {
             this.loader = loader;
             scene = loader.loadSceneFromFile("Padawans_media\\XWing\\TRENCH_RUN-TgcScene.xml");
+            escena_sin_paredes = loader.loadSceneFromFile("Padawans_media\\XWing\\death+star-TgcScene.xml");
             this.n = n;
+        }
+
+        private void PlaceMeshLine(TgcScene escena,TGCVector3 posicion, TGCVector3 escalador,int repeticiones,int mesh_pivot)
+        {
+            for (int i = 0; i < repeticiones; i++)
+            {
+                foreach (TgcMesh mesh in escena.Meshes)
+                {
+                    mesh.Position = posicion;
+                    mesh.Scale = escalador;
+                }
+
+                posicion = new TGCVector3(  escena.Meshes[mesh_pivot].Position.X,
+                                            escena.Meshes[mesh_pivot].Position.Y,
+                                            escena.Meshes[mesh_pivot].Position.Z -
+                                                escena.Meshes[mesh_pivot].BoundingBox.calculateSize().Z);
+                
+                escena.RenderAll();
+            }
         }
 
         public override void Render()
         {
-            TGCVector3 posicionIncremental = new TGCVector3(0,0,0);
-            for (int i=0;i<n;i++)
-            {
-                posicionIncremental.Z = - i * 100;
-                foreach (TgcMesh mesh in scene.Meshes)
-                {
-                    mesh.Position = posicionIncremental;
-                }
-                scene.RenderAll();
-            }
+
+            var posicion = new TGCVector3(0f, 0f, 0f);
+            var escalador = new TGCVector3(30f, 30f, 30f);
+            int mesh_pivot = 0;
+            PlaceMeshLine(scene, posicion,escalador,n,mesh_pivot);
+
+            posicion = new TGCVector3(-50f, 0f, 0f);
+            mesh_pivot = 1;
+            PlaceMeshLine(escena_sin_paredes, posicion, escalador, n, mesh_pivot);
+
         }
 
         public override void Update()
@@ -52,11 +72,13 @@ namespace TGC.Group.Model
         public override void Dispose()
         {
             scene.DisposeAll();
+            escena_sin_paredes.DisposeAll();
         }
 
         public override void RenderBoundingBox()
         {
             scene.BoundingBox.Render();
+            escena_sin_paredes.BoundingBox.Render();
         }
     }
 }
