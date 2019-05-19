83224c9f5ddaa58ab3feffeaaecc78a90fffaac5
Franco <31745377+Bade99@users.noreply.github.com>
2019-05-19 13:00:26 -0300

    Add files via upload

---
+++ Padawans/Model/VariablesGlobales.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TGC.Group.Model
+{
+    public static class VariablesGlobales
+    {
+        public static string mediaDir;
+    }
+}
