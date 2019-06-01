200bef90b2a772c806b94579d910bfc3816212d7
Bade99 <31745377+Bade99@users.noreply.github.com>
2019-06-01 15:34:15 -0300

    ya se puede hacer mute desde el menu pausa, y tiene icono

--- Padawans/Model/PauseMenu.cs
+++ Padawans/Model/PauseMenu.cs
@@ -17,8 +17,21 @@ namespace TGC.Group.Model
 {
     class PauseMenu : IMenu //la idea es que el menu de pausa sea semi opaco, con color negro, y se vea atras el juego, no se como hacerlo por ahora
     {
+        //Main window
         private CustomSprite fondo;
-        private CustomBitmap bitmap;
+        private CustomBitmap bitmap_fondo;
+        //
+        private struct Rect
+        {
+            public TGCVector2 top_right, bottom_left, bottom_right;
+        }
+        private Rect main_rect;
+
+        //Sound icon
+        private CustomSprite sound;
+        private CustomBitmap bitmap_sound;
+        private CustomBitmap bitmap_sound_off;
+        //
         private Drawer2D drawer2D;
         private bool isCurrent = false;
         private Key mappedKey;
@@ -29,10 +42,15 @@ namespace TGC.Group.Model
             this.mappedKey = mappedKey;
             drawer2D = new Drawer2D();
             fondo = new CustomSprite();
-            bitmap = new CustomBitmap(VariablesGlobales.mediaDir + "Bitmaps\\pause_menu.png", D3DDevice.Instance.Device);
-            fondo.Bitmap = bitmap;
+            bitmap_fondo = new CustomBitmap(VariablesGlobales.mediaDir + "Bitmaps\\pause_menu.png", D3DDevice.Instance.Device);
+            fondo.Bitmap = bitmap_fondo;
             CalcularFullScreenScalingAndPosition(fondo);
-        }
+            sound = new CustomSprite();
+            bitmap_sound = new CustomBitmap(VariablesGlobales.mediaDir + "Bitmaps\\sound_icon.png", D3DDevice.Instance.Device);
+            bitmap_sound_off = new CustomBitmap(VariablesGlobales.mediaDir + "Bitmaps\\sound_off_icon.png", D3DDevice.Instance.Device);
+            CalcularChildScalingAndPosition(sound,bitmap_sound/*lo uso como base, total el de sound on y off son iguales*/
+                ,.90f,.90f,.05f,.1f,main_rect);//90% de x 90% de y , .5% del tamaño de la pantalla
+        }//@ ver si hay alguna forma de no tener q pasar los dos scale, usar aspect_ratio del rect capaz
 
         public bool CheckStartKey(TgcD3dInput input)
         {
@@ -68,6 +86,12 @@ namespace TGC.Group.Model
 
             //Dibujar sprite (si hubiese mas, deberian ir todos aquí)
             drawer2D.DrawSprite(fondo);
+            
+            //Sound icon
+            if (VariablesGlobales.SOUND) sound.Bitmap = bitmap_sound;
+            else sound.Bitmap = bitmap_sound_off;
+            drawer2D.DrawSprite(sound);
+            //
 
             //Finalizar el dibujado de Sprites
             drawer2D.EndDrawSprite();
@@ -81,25 +105,52 @@ namespace TGC.Group.Model
         {
             return isCurrent;
         }
-        public void CalcularFullScreenScalingAndPosition(CustomSprite fondo)
+        private void CalcularFullScreenScalingAndPosition(CustomSprite main)
         {//para 1280x720 lo transforma en 2048x1024 (no tiene mucha precision)
-            var tamanio_textura = fondo.Bitmap.Size;
-            fondo.ScalingCenter = new TGCVector2(0, 0);
+            var tamanio_textura = main.Bitmap.Size;
+            main.ScalingCenter = new TGCVector2(0, 0);
             if (D3DDevice.Instance.AspectRatio < 2.1f)//16:9 o parecido
             {
-                fondo.Position = new TGCVector2(0, 0);
+                main.Position = new TGCVector2(0, 0);
 
                 float width = (float)D3DDevice.Instance.Width / (float)tamanio_textura.Width;
                 float height = (float)D3DDevice.Instance.Height / (float)tamanio_textura.Height;
-                fondo.Scaling = new TGCVector2(width, height);
+                main.Scaling = new TGCVector2(width, height);
             }
             else //21:9 o parecido
             {
-                fondo.Position = new TGCVector2(((float)D3DDevice.Instance.Width - ((float)D3DDevice.Instance.Width * 16f / 21.6f)) / 2, 0);
+                main.Position = new TGCVector2(((float)D3DDevice.Instance.Width - ((float)D3DDevice.Instance.Width * 16f / 21.6f)) / 2, 0);
                 float width = ((float)D3DDevice.Instance.Width * 16f / 21.6f) / (float)tamanio_textura.Width;
                 float height = (float)D3DDevice.Instance.Height / (float)tamanio_textura.Height;
-                fondo.Scaling = new TGCVector2(width, height);
+                main.Scaling = new TGCVector2(width, height);
             }
+            var tr_x = main.Position.X + tamanio_textura.Width * main.Scaling.X;
+            var tr_y = main.Position.Y;
+            main_rect.top_right = new TGCVector2(tr_x,tr_y);
+            var bl_x = main.Position.X;
+            var bl_y = main.Position.Y + tamanio_textura.Height * main.Scaling.Y;
+            main_rect.bottom_left = new TGCVector2(bl_x,bl_y);
+
+            var br_x = tr_x;
+            var br_y = bl_y;
+            main_rect.bottom_right = new TGCVector2(br_x,br_y);
+            //@asignar main_rect not so easy to do
+        }
+        private void CalcularChildScalingAndPosition(CustomSprite child,CustomBitmap bitmap_base,float relX,float relY,float relScaleX, float relScaleY, Rect main)
+        {
+            float posX = main.bottom_right.X - main.bottom_left.X;
+            float posY = main.bottom_right.Y - main.top_right.Y;
+            child.Position = new TGCVector2(posX * relX, posY * relY);
+            child.Scaling = CalculeRelativeScaling(bitmap_base, posX,posY,relScaleX,relScaleY);//@capaz usar un scale para cada eje?
+        }
+        private TGCVector2 CalculeRelativeScaling(CustomBitmap bitmap,float width,float height, float scale_x,float scale_y)
+        {//hace el calculo sobre width
+            float screen_occupation_x = width * scale_x;
+            float screen_occupation_y = height * scale_y;
+            //float screen_ratio = D3DDevice.Instance.Width / bitmap.Width;
+            float escala_real_x = screen_occupation_x / bitmap.Width;
+            float escala_real_y = screen_occupation_y / bitmap.Height;
+            return new TGCVector2(escala_real_x, escala_real_y);
         }
     }
 }
